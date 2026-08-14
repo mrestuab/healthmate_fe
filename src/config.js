baac89564f17de7170922849e58fb89543a5f91e
mrestuab <mrestuab54@gmail.com>
2026-08-14 17:12:38 +0700

    feat: add config file to manage base API URLs for environment-specific endpoints

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const devAPIURL = "http://127.0.0.1:5000"
 // const devAPIURL = "http://localhost:3000" //ini dev vercel
-const prodAPIURL = "https://healthmate-be-jade.vercel.app"
+const prodAPIURL = "https://healthmate-be-sigma.vercel.app"
 const isProd = true
 
 export const getBaseUrl = (url) => {
